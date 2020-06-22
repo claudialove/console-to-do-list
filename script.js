7cb4cca5e2bda2367a7d17563582c9ba94a9dc1f
Claudia Petty <claudialove32@gmail.com>
2020-06-22 12:44:01 -0400

    add while loop and remove function

--- script.js
+++ script.js
@@ -3,26 +3,36 @@ window.setTimeout(function () {
 
     //need a prompt to ask user what they would like to do
     //make it a function though so you can reuse it
-    function start() {
-        var userEntry = prompt("Commands for this program are: LIST, NEW, QUIT ...what would you like to do?");
+
+        //need to declare a variable that is an array
+        var myTasks = ["feed the cat", "love the cat", "groom the cat"];
+        var userEntry = prompt("Commands for this program are: LIST, NEW, COMPLETE, QUIT ...what would you like to do?");
      
-            //need to declare a variable that is an array
-            var myTasks = ["feed the cat", "love the cat", "groom the cat"];
+while (userEntry !== "quit") {
 
             //if user types list...
             if (userEntry === "list") {
                 //console.log([todo-array])
                 console.log(myTasks);
-                start();
             }
 
             //if user types new...
             else if (userEntry === "new") {
                 //prompt user to enter new todo
                 var newTask = prompt("ADD TASK... ");
-                console.log(myTasks.push(newTask));
-                console.log(myTasks);
-                start();
+                //add new task to myTasks
+                myTasks.push(newTask);
+            }
+
+            //if user types complete...
+            else if (userEntry === "complete") {
+                //let user type the task they want to delete
+                var deleteTask = prompt("What task have you completed? I will remove it from the list!");
+                //loop over the array to match it
+               for (var i = 0; i < myTasks.length; i ++) {
+                   if (i === deleteTask);
+                   myTasks.splice(i, 1);
+               }
             }
 
             //if user types quit...
@@ -30,9 +40,10 @@ window.setTimeout(function () {
                 //console.log ("goodbye") and end program
                 console.log("goodbye");
             } else {
-                alert("INVALID COMMAND!! Use LIST or NEW  or QUIT");
-                start();
+                alert("INVALID COMMAND!! Use LIST or NEW or COMPLETE  or QUIT");
             }
-    }
-    start();
-}, 10000);
+            var userEntry = prompt("Commands for this program are: LIST, NEW, COMPLETE, QUIT ...what would you like to do?");
+}
+console.log("goodbye")
+
+}, 5000);
